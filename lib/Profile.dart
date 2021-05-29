c1a45b0c00e2dac8353a6f4f1ec69b0ef1870711
Mahmoudoe <Mahmoudaafh@gmail.com>
2021-05-30 01:35:08 +0200

    GP Task V.1.0

--- lib/Profile.dart
+++ lib/Profile.dart
@@ -11,17 +11,16 @@ class Profile extends StatefulWidget {
 }
 class ProfileState extends State<Profile> {
   Widget img = FlutterLogo();
-  String nickName = "Nickname ";
-  String address = "Address ";
-  String phoneNum = "Phone Number ";
+  String nickName = "Ro3b";
+  String address = "Cairo";
+  String phoneNum = "0112358 ";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold( backgroundColor: Color(0xffffffff),
-      body: Center(child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: Column(mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          SizedBox(height: 20.0,),
+          SizedBox(height: 10.0,),
 
           Container(
             margin: EdgeInsets.all(25),
@@ -34,10 +33,10 @@ class ProfileState extends State<Profile> {
                     borderColor: Color(0xffA8A8A8),
                     child: img,
                     elevation: 5,
-                    radius: 100,
+                    radius: 75,
                   ),
 
-                SizedBox(height: 50.0,),
+                SizedBox(height: 25.0,),
 
                 Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [SizedBox(width: 25.0,),
@@ -61,16 +60,47 @@ class ProfileState extends State<Profile> {
             ),
           ),
 
+          Divider(
+            height: 5,
+            thickness: 2.5,
+            indent: 75,
+            endIndent: 75,
+            color: Color(0xffA8A8A8),
+          ),
+
+          SizedBox(height: 5.0,),
+
+          Row(children: [
+              SizedBox(width:7.5 ,),
+              Text("Profile Info: ", style:TextStyle(fontSize: 25.0,fontWeight: FontWeight.bold),),
+            ],
+          ),
+
           Container(
-              margin: EdgeInsets.all(25),
-              padding:EdgeInsets.all(20),
+              margin: EdgeInsets.fromLTRB(15, 20, 15, 20) ,
+              padding:EdgeInsets.all(10),
               child: Column(mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                Text(nickName, style:TextStyle(fontSize: 30.0,),),
-                  SizedBox(height: 75.0,),
-                  Text(address, style:TextStyle(fontSize: 30.0,),),
-                  SizedBox(height: 75.0,),
-                Text(phoneNum, style:TextStyle(fontSize: 30.0,),),
+                  Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("Nickname: ", style:TextStyle(fontSize: 25.0,),),
+                      Text(nickName, style:TextStyle(fontSize: 20.0,),),
+                    ],
+                  ),
+                  SizedBox(height: 50.0,),
+                  Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("Address: ", style:TextStyle(fontSize: 25.0,),),
+                      Text(address, style:TextStyle(fontSize: 20.0,),),
+                    ],
+                  ),
+                  SizedBox(height: 50.0,),
+                  Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("phoneNum: ", style:TextStyle(fontSize: 25.0,),),
+                      Text(phoneNum, style:TextStyle(fontSize: 20.0,),),
+                    ],
+                  ),
           ],)
           ),
 
@@ -78,7 +108,6 @@ class ProfileState extends State<Profile> {
 
         ],
         ),
-      ),
     );
   }
 }
